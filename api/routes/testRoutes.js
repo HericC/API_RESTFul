5e6e7d6715ee403580e919d15b1bcd84793e0186
Heric Macedo Oliveira Silva <heric_commercial@hotmail.com>
2020-06-29 23:52:28 -0300

    change: added token route on test routes

--- api/routes/testRoutes.js
+++ api/routes/testRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const testController = require('../controllers/testController');
 
+router.get('/token', testController.token);
 router.get('/select', testController.select);
 router.post('/insert', testController.insert);
 
